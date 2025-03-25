d7eb3a7d0927d7cd04116ddc2aabc08567368650
Afriza <140886763+afrzchrs@users.noreply.github.com>
2025-03-25 12:05:17 +0700

    Update dashboard_lihatJadwalKereta.py

--- ui/dashboard_lihatJadwalKereta.py
+++ ui/dashboard_lihatJadwalKereta.py
@@ -1,7 +1,11 @@
-# Nama  : Afriza Choirie Saputra
-# NIM   : 241524002
-# Kelas : D4-TIF/1A
-# Progress : Melakukan beberapa perubahan ui 
+"""
+Author: Afriza Choirie Saputra
+NIM: 241524002
+Kelas: 1A
+Prodi: Sarjana Terapan Teknik Informatika
+Jurusan: Teknik Komputer dan Informatika
+Politeknik Negeri Bandung
+"""
 
 # -*- coding: utf-8 -*-
 
