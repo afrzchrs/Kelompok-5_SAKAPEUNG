87eb05b2d68e16d290cf6c43663158d558e8e6b1
Adirafi-22 <adirafi22@gmail.com>
2025-03-25 11:46:25 +0700

    lihatRekapKeuntungan_dasboard.py

--- screens/lihatRekapKeuntungan_dasboard.py
+++ screens/lihatRekapKeuntungan_dasboard.py
@@ -1,3 +1,12 @@
+"""
+Nama: Adi Rafi Chaerufarizki
+NIM: 241524001
+Kelas: 1A
+Prodi: Sarjana Terapan Teknik Informatika
+Jurusan: Teknik Komputer dan Informatika
+Politeknik Negeri Bandung
+"""
+
 import json
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 from ui.lihat_RekapKeuntungan import Ui_lihat_RekapKeuntungan as lihat_RekapKeuntungan
