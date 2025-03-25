99994707e6c2ecd0e84daf1c469f7b4f6ad97ae0
Afriza <140886763+afrzchrs@users.noreply.github.com>
2025-03-25 12:04:36 +0700

    Update EditDataKereta_dashboard.py

--- screens/EditDataKereta_dashboard.py
+++ screens/EditDataKereta_dashboard.py
@@ -1,7 +1,11 @@
-# Nama  : Afriza Choirie Saputra
-# NIM   : 241524002
-# Kelas : D4-TIF/1A
-# Progress : Melakukan pemisahan program yang digunakan berulang dan melakukan beberapa perubahan fungsionalitas program
+"""
+Author: Afriza Choirie Saputra
+NIM: 241524002
+Kelas: 1A
+Prodi: Sarjana Terapan Teknik Informatika
+Jurusan: Teknik Komputer dan Informatika
+Politeknik Negeri Bandung
+"""
 
 import json
 import os
